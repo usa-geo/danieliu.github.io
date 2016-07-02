0e5c4fd211fb7f594c6cacdbf274e8ca4bf7e57b
Daniel Liu <idaniel.liu@gmail.com>
2016-07-02 14:24:34 -0700

    Claudia counter reaching 0 embed

--- claudia/claudia.js
+++ claudia/claudia.js
@@ -9,7 +9,7 @@
     var units = ['day', 'hour', 'minute', 'second'];
 
     // Calculate number of seconds
-    var tdelta = parseInt((date - new Date(Date.now())) / 1000);
+    var tdelta = parseInt((date - Date.now()) / 1000);
 
     function getRemainingTime(time) {
         days = parseInt(time / (60 * 60 * 24), 10);
@@ -33,15 +33,38 @@
         }
     }
 
+    function embedGoodbye() {
+        var container = document.querySelector('.countdown'),
+            embed = document.createElement('iframe'),
+            w = window.innerWidth,
+            attrs = {
+                src: 'https://www.youtube.com/embed/s0SUEMGZU04?autoplay=1&controls=0&showinfo=0&rel=0',
+                frameborder: 0,
+                allowfullscreen: ''
+            };
+
+        attrs.width = w < 500 ? w : 500;
+        attrs.height = attrs.width * 3 / 4;
+
+        for (var key in attrs) {
+            embed.setAttribute(key, attrs[key]);
+        }
+
+        container.replaceChild(embed, countdown);
+    }
+
     if (tdelta > 0) {
         // Initialize to show values immediately
         updateCounters(counters, labels, units, getRemainingTime(tdelta--));
 
         var timer = setInterval(function () {
             updateCounters(counters, labels, units, getRemainingTime(tdelta--));
-            if (tdelta < 0) {
+            if (tdelta <= 0) {
                 clearInterval(timer);
+                embedGoodbye();
             }
         }, 1000);
+    } else {
+        embedGoodbye();
     }
 })();
